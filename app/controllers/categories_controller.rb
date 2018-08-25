e2d2732c166a48d3c7f42f9394213c7ae2e15b3f
Pat <pat.mccartney2@gmail.com>
2018-08-25 14:33:54 -0600

    functioning delete

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -46,6 +46,14 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+
+    flash[:success] = "#{category.title} was successfully deleted!"
+    redirect_to categories_path
+  end
+
 
   private
 
--- spec/features/categories/user_can_delete_and_edit_from_cat_index_spec.rb
+++ spec/features/categories/user_can_delete_and_edit_from_cat_index_spec.rb
@@ -21,5 +21,16 @@ describe 'user can edit a category' do
     expect(current_path).to eq(category_path(category))
     expect(page).to have_content("Seamer")
   end
-
 end
+  describe 'user can delete a category from category index' do
+    scenario 'user clicks delete and is redirected to updated index page' do
+      category = Category.create(title: "Beet Farmer")
+
+      visit categories_path
+
+      click_on("Delete")
+
+      expect(current_path).to eq(categories_path)
+      expect(page).to have_content("#{category.title} was successfully deleted!")
+    end
+  end
